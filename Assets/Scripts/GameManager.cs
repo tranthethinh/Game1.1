66c43db977b28056144afd93ba5fe6cf8413ba2f
tranthethinh <tranthethinh2001@gmail.com>
2023-05-06 23:19:51 +0700

    game1.1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public int score=0;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI gameWinText;
+    public Button restartButton;
+
+    public GameObject[] ballPrefabs = new GameObject[3];
+    private float spawnRange = 22;
+    private float ySpawn = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        score = 0;
+        for (int i = 0; i < ballPrefabs.Length; i++)
+        {
+            Instantiate(ballPrefabs[i], RandomPos(), ballPrefabs[i].gameObject.transform.rotation);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpawnBall1second(string inputTag)
+    {
+        // Find ball prefabs that have the input tag
+        GameObject taggedPrefabs = System.Array.Find(ballPrefabs, prefab => prefab.CompareTag(inputTag));
+        if (taggedPrefabs !=null)
+        {
+            StartCoroutine(SpawnBallDelayed(taggedPrefabs, 1f));
+        }
+    }
+
+    private IEnumerator SpawnBallDelayed(GameObject prefab, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SpawnBall(prefab);
+    }
+    public void SpawnBall(GameObject ball)
+    {
+        Vector3 spawnPos = RandomPos();
+
+        Instantiate(ball, spawnPos, ball.gameObject.transform.rotation);
+    }
+    public Vector3 RandomPos()
+    {
+        float randomX = Random.Range(-spawnRange, spawnRange);
+        float randomZ = Random.Range(-spawnRange, spawnRange);
+        int randomIndex = Random.Range(0, ballPrefabs.Length);
+    
+        return new Vector3(randomX, ySpawn, randomZ);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score:" + score;
+        if (score >= 10)
+        {
+            GameWin();
+        }
+    }
+    public void GameWin()
+    {
+        gameWinText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 100.0f;
+    private Rigidbody playerRb;
+    public bool isGameOver=false;
+
+    public Color[] colors = { Color.red, Color.green, Color.blue };
+
+    HashSet<string> validBallTags = new HashSet<string>() { "RedBall", "BlueBall", "GreenBall" };
+
+    GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        //make the player stand upright
+        playerRb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        gameManager= GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        int randomIndex = Random.Range(0, colors.Length);
+        Color randomColor = colors[randomIndex];
+        GetComponent<Renderer>().material.color = randomColor;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovePlayer();
+        
+    }
+    void MovePlayer()
+    {
+        if (!isGameOver||gameManager.score<10)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+            playerRb.AddForce(movement * speed);
+        }
+    }
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.CompareTag("Ball"))
+        if (validBallTags.Contains(collision.gameObject.tag))
+        {
+            if (GetComponent<Renderer>().material.color == collision.gameObject.GetComponent<Renderer>().material.color)
+            {
+                int randomIndex = Random.Range(0, colors.Length);
+                Color randomColor = colors[randomIndex];
+                GetComponent<Renderer>().material.color = randomColor;
+                Destroy(collision.gameObject);
+                gameManager.SpawnBall1second(collision.gameObject.tag);
+                gameManager.UpdateScore(1);
+            }
+            else
+            {
+                isGameOver = true;
+                gameManager.GameOver();
+
+            }
+        }
+    }
+}
